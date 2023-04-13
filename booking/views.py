2f5931ef512565c3dc525d733c7e881f5f8070aa
adrian-cucuet <adrian.cucuet@hotmail.com>
2023-04-13 20:18:58 +0000

    Create meal list view

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Meals
 
 # Create your views here.
+
+def meal_list(request):
+    meal_list = booking.objects.all()
+
+    context = {'meal_list' : meal_list ,}
+
+    return render(request , 'booking/list.html' , context)
+
+
+def meal_detail(request, slug):
+    pass
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -22,3 +22,12 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('booking/', include('booking.urls', namespace='booking')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
