366b678a876fdd77b3ccaccfa836b49b6a9d4dec
adrian-cucuet <adrian.cucuet@hotmail.com>
2023-04-13 19:39:43 +0000

    Create meal option in admin and slugify install

--- booking/models.py
+++ booking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -8,7 +9,23 @@ class Meals(models.Model):
     description = models.TextField(max_length=500)
     people = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    preparation_time = models.IntegField()
+    preparation_time = models.IntegerField()
     image = models.ImageField(upload_to='booking/img/')
+    slug = models.SlugField(blank=True, null=True)
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug and self.name:
+            self.slug = slugify(self.name)
+        super(Meals, self).save(*args, **kwargs)
+
+
+    class Meta:
+        verbose_name = 'Meal'
+        verbose_name_plural = 'Meals'
+
+
+    def __str__(self):
+        return self.name
 
 
--- restaurant/settings.py
+++ restaurant/settings.py
@@ -118,7 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_DIR = BASE_DIR / 'media'
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
