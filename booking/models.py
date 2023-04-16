5cb780190f40e428ecb7d77c371c6a06e51ff84c
adrian-cucuet <adrian.cucuet@hotmail.com>
2023-04-16 00:36:53 +0000

    Add meal category template and loop in list template for menu frontend

--- booking/models.py
+++ booking/models.py
@@ -7,6 +7,7 @@ from django.utils.text import slugify
 class Meals(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
     people = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     preparation_time = models.IntegerField()
@@ -29,3 +30,14 @@ class Meals(models.Model):
         return self.name
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+
+    def __str__(self):
+        return self.name
